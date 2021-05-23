d91e0e28371d9a27ae981981737e1a907ad5c430
Oceanity <github@oceanity.net>
2021-05-22 22:10:30 -0500

    Optimized cover art downloading

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const WebUI = require("./lib/WebUI");
+const WebUI = require("./lib/WebUI"),
+    PlexReader = require("./lib/PlexReader");
 
 function Init(options) {
+    options.plexReader = new PlexReader(options.plex);
     const ui = new WebUI(options);
 }
 module.exports = Init;
